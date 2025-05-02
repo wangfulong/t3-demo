f3439dcdf488b5c6671e715129dc32327f2e584b
wangfulong.eth <wfllike@qq.com>
2025-05-02 15:10:52 +0800

    Initial commit

---
+++ apps/expo/src/utils/api.tsx
@@ -0,0 +1,49 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createTRPCClient, httpBatchLink, loggerLink } from "@trpc/client";
+import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import superjson from "superjson";
+
+import type { AppRouter } from "@acme/api";
+
+import { getBaseUrl } from "./base-url";
+import { getToken } from "./session-store";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // ...
+    },
+  },
+});
+
+/**
+ * A set of typesafe hooks for consuming your API.
+ */
+export const trpc = createTRPCOptionsProxy<AppRouter>({
+  client: createTRPCClient({
+    links: [
+      loggerLink({
+        enabled: (opts) =>
+          process.env.NODE_ENV === "development" ||
+          (opts.direction === "down" && opts.result instanceof Error),
+        colorMode: "ansi",
+      }),
+      httpBatchLink({
+        transformer: superjson,
+        url: `${getBaseUrl()}/api/trpc`,
+        headers() {
+          const headers = new Map<string, string>();
+          headers.set("x-trpc-source", "expo-react");
+
+          const token = getToken();
+          if (token) headers.set("Authorization", `Bearer ${token}`);
+
+          return Object.fromEntries(headers);
+        },
+      }),
+    ],
+  }),
+  queryClient,
+});
+
+export { type RouterInputs, type RouterOutputs } from "@acme/api";
